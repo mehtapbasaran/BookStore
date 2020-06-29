2d4c67cf123cc0d1a0456aa879fac3420710dea4
mehtapbasaran <mehtap_gs_95@hotmail.com>
2020-06-29 17:34:48 +0300

    Order Management Operations

---
+++ BookStore.Models/ViewModels/OrderDetailsVM.cs
@@ -0,0 +1,13 @@
+﻿using BookStore.Models.DbModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookStore.Models.ViewModels
+{
+    public class OrderDetailsVM
+    {
+        public OrderHeader OrderHeader { get; set; }
+        public IEnumerable<OrderDetails> OrderDetails { get; set; }
+    }
+}
